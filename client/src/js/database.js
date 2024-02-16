5e808491d68e2481527b968327efdf6e9482e192
Jsaini1727 <jsaini1727@gmail.com>
2024-02-16 16:32:51 -0500

    Completed installing the package and did the coding

---
+++ client/src/js/database.js
@@ -0,0 +1,47 @@
+import { openDB } from 'idb';
+
+const initdb = async () =>
+  openDB('jate', 1, {
+    upgrade(db) {
+      if (db.objectStoreNames.contains('jate')) {
+        console.log('jate database already exists');
+        return;
+      }
+      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
+      console.log('jate database created');
+    },
+  });
+
+// TODO: Add logic to a method that accepts some content and adds it to the database
+export const putDb = async (content) => {
+  try {
+    const textEditDb = await openDB('jate', 1);
+    const txt = textEditDb.transaction('jate', 'readwrite');
+    const storeTxt = txt.objectStore('jate');
+    const request = storeTxt.put({
+      value: content,
+      id: 1
+    })
+    const output = await request;
+    console.log('Data added to DB', output)
+  } catch (error) {
+    console.error('putDb not implemented')
+  }
+};
+
+// TODO: Add logic for a method that gets all the content from the database
+export const getDb = async () => {
+  try {
+    const textEditDb = await openDB('jate', 1);
+    const txt = textEditDb.transaction('jate', 'readonly');
+    const storeTxt = txt.objectStore('jate');
+    const request = storeTxt.get(1);
+    const output = await request;
+    console.log('output.value', output)
+    return output?.value;
+  } catch (error) {
+    console.error('getDb not implemented')
+  }
+};
+
+initdb();
---
+++ client/webpack.config.js
@@ -0,0 +1,70 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const WebpackPwaManifest = require('webpack-pwa-manifest');
+const path = require('path');
+const { InjectManifest } = require('workbox-webpack-plugin');
+
+// TODO: Add and configure workbox plugins for a service worker and manifest file.
+// TODO: Add CSS loaders and babel to webpack.
+
+module.exports = () => {
+  return {
+    mode: 'development',
+    entry: {
+      main: './src/js/index.js',
+      install: './src/js/install.js'
+    },
+    output: {
+      filename: '[name].bundle.js',
+      path: path.resolve(__dirname, 'dist'),
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'Webpack 11'
+        // filename: 'index.html',
+        // chunks: ['main'],
+      }),
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js',
+        // additionalManifestEntries: [
+        //   { url: 'index.html', revision: null },
+        // ],
+      }),
+      new WebpackPwaManifest({
+        name: 'TextEditor_PWA',
+        short_name: 'TEPWA',
+        description: 'Notes and editing them online or offline',
+        background_color: 'lightgreen',
+        theme_color: 'blue',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons')
+          },
+        ],
+      }),
+    ],
+
+    module: {
+      rules: [
+        {
+          test: /\.css$/i,
+          use: ["style-loader", "css-loader"],
+        },
+        {
+          test: /\.(?:js|mjs|cjs)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime']
+            }
+          }
+        },
+      ],
+    },
+  };
+};
